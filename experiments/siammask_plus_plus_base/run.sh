b2980ca38d4272d18e6fed916ea15eb1ede7b4f0
Hyunbin-Choi <55519278+wowhb@users.noreply.github.com>
2021-05-25 17:27:41 +0900

    Add files via upload

---
+++ experiments/siammask_plus_plus_base/run.sh
@@ -0,0 +1,12 @@
+ROOT=`git rev-parse --show-toplevel`
+export PYTHONPATH=$ROOT:$PYTHONPATH
+
+mkdir -p logs
+
+python -u $ROOT/tools/train_siammask_pp.py \
+    --config=config.json -b 16 \
+    -j 4 \
+    --epochs 20 \
+    --log logs/log.txt \
+    2>&1 | tee logs/train.log
+
