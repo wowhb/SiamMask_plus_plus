0c7535f3d35db593c140f8f8390f5ec0ef60be74
C H-B <55519278+wowhb@users.noreply.github.com>
2021-08-06 17:39:43 +0900

    Update resnet.py

--- experiments/re_siammask_plus_plus/resnet.py
+++ experiments/re_siammask_plus_plus/resnet.py
@@ -9,7 +9,6 @@ from utils.log_helper import log_once
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'resnet152']
 
-
 model_urls = {
     'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
     'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
@@ -17,6 +16,7 @@ model_urls = {
     'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
     'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
 }
+
 
 
 def conv3x3(in_planes, out_planes, stride=1):
